c89a1c2a473880da106efdbdde1a052b0412244f
MargoStranger <margosebastian13@gmail.com>
2023-07-09 19:28:53 +0300

    сделала 2 дз

---
+++ Dz2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+//Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int minValue  =  0;
+int maxValue = 10;
+
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+
+GetCountElement(array, minValue, maxValue);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetCountElement(int[,] inArray, int minValue, int maxValue)
+{
+    for (int k = minValue; k < maxValue; k++)
+    {
+        int count = 0;
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inArray.GetLength(1); j++)
+            {
+                count += inArray[i,j] == k ? 1:0;
+
+            }
+        }
+        if(count != 0 ) Console.WriteLine($"{k} встречается {count} раз");
+    }
+}
